01cf2b8814141aa0a0ddf252fd1f24fa5dbd2d3b
me@nowhere.com <me@nowhere.com>
2020-04-23 07:07:11 -0600

    Update lexer error handling

--- h2/test_lexer.py
+++ h2/test_lexer.py
@@ -4,6 +4,7 @@
 #
 
 import sys
+import ast
 
 from sly import Lexer
 
@@ -15,12 +16,13 @@ class TestLexer(Lexer):
     # String containing ignored characters (between tokens)
     ignore = ' \t'
 
-    # Other ignored patterns
-    ignore_comment = r'\#.*'
+    # Other ignored patterns. See rule below
+    #ignore_comment = r'\#.*'
 
-    reserved_words = {'PRINT', 'BEGIN', 'END'}
+    # token values that conflict with 'ID' token rule
+    reserved_words = {'Print', 'Do', 'Done', 'Mission', 'True', 'False', 'Not'}
 
-    # Set of token names.   This is always required
+    # Set of token types.   This is always required
     tokens = {
         'ID',
         'NUMBER',
@@ -41,43 +43,69 @@ class TestLexer(Lexer):
         'LBRACKET',
         'RBRACKET',
         'COMMA',
-        'COLON',
-        'PERIOD',
+        'MISSION',
+        'PRINT',
+        'BLOCK_BEGIN',
+        'BLOCK_END',
+        'NOT',
+        'COMMENT',
+        'ENDLINE',
     }
 
-    # see https://stackoverflow.com/questions/2039795/regular-expression-for-a-string-literal-in-flex-lex
+    # see stackoverflow.com/questions/2039795/regular-expression-for-a-string-literal-in-flex-lex
 
     # Regular expression rules for tokens
-    ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    STRING  = r'\"(\\.|[^"\\])*\"'
-    PLUS    = r'\+'
-    MINUS   = r'-'
-    TIMES   = r'\*'
-    DIVIDE  = r'/'
-    LPAREN  = r'\('
-    RPAREN  = r'\)'
-    BOOL    = r'True|False'
-    EQ      = r'=='       # MUST APPEAR FIRST! (LONGER)
-    ASSIGN  = r'='
-    GE      = r'>='
-    LE      = r'<='
-    GT      = r'>'
-    LT      = r'<'
-    LBRACKET= r'\['
-    RBRACKET= r'\]'
-    COMMA   = r','
-    COLON   = r':'
-    PERIOD  = r'\.'
+    ID          = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING      = r'\"(\\.|[^"\\])*\"'      # capture entire multi-line strings to simplify error handling
+    BOOL        = r'True|False'             # see ID below
+    PLUS        = r'\+'
+    MINUS       = r'-'
+    TIMES       = r'\*'
+    DIVIDE      = r'/'
+    EQ          = r'=='                     # MUST APPEAR BEFORE '=' (LONGER)
+    ASSIGN      = r'='
+    GE          = r'>='
+    LE          = r'<='
+    GT          = r'>'
+    LT          = r'<'
+    LPAREN      = r'\('
+    RPAREN      = r'\)'
+    LBRACKET    = r'\['
+    RBRACKET    = r'\]'
+    COMMA       = r','
+    MISSION     = r'Mission'
+    PRINT       = r'Print'
+    NOT         = r'Not'
+    BLOCK_BEGIN = r'Do'
+    BLOCK_END   = r'Done'
+    COMMENT     = r'\#.*'
+    ENDLINE     = r'\n'
 
     @_(r'\d+')
     def NUMBER(self, token):
         token.value = int(token.value)   # Convert to a numeric value
         return token
 
+    @_(r'\"(\\.|[^"\\])*\"')
+    def STRING(self, token):
+        try:
+            token.value = ast.literal_eval(token.value) # Convert to an actual string vs string-in-a-string
+        except Exception as e:
+            print("{}: {} on line {}, char {}. Context: {}".format(e.__class__.__name__, e.msg, self.lineno, e.offset, e.text))
+
+        return token
+
     def ID(self, token):
         if token.value in self.reserved_words:
-            token.type = token.value.upper()
-            #print("Keyword {} at line {} char {}".format(token.type, self.lineno + 1, self.find_column(token.value, token)))
+            if token.value == 'True' or token.value == 'False':
+                token.type = 'BOOL'
+                token.value =  bool(token.value)
+            elif token.value == 'Do':
+                token.type = 'BLOCK_BEGIN'
+            elif token.value == 'Done':
+                token.type = 'BLOCK_END'
+            else:
+                token.type = token.value.upper()
         return token
 
     # Compute column.
@@ -92,9 +120,11 @@ class TestLexer(Lexer):
         return column
 
     # Define a rule so we can track line numbers
-    @_(r'\n+')
-    def ignore_newline(self, token):
-        self.lineno += token.value.count('\n')
+    @_(r'\n')
+    def ENDLINE(self, token):
+        self.lineno += 1
+        token.value = 'ENDLINE'
+        return token
 
     def error(self, token):
         print("Line {}: Bad character '{}' at char {}".format(self.lineno, token.value[0], self.find_column(token.value[0], token)))
@@ -118,6 +148,12 @@ def main():
             try:
                 text = input()
                 for tok in lexer.tokenize(text):
+                    if not Show_endlines and tok.type == 'ENDLINE':
+                        continue
+
+                    if not Show_comments and tok.type == 'COMMENT':
+                        continue
+
                     print('type=%r, value=%r' % (tok.type, tok.value))
             except EOFError:
                 break
